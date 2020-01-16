6f7125500ec11c98ee220d6676a51547061b6d40
Issam SAID <isaid@nvidia.com>
2020-01-16 14:37:07 +0400

    add the ubuntu scripts

---
+++ build_ubuntu.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -ex
+
+SLURM_VERSION=19.05.0
+UBUNTU_CODENAME='bionic'
+
+#UBUNTU_CODENAMES='xenial bionic'
+CENTOS_RELEASES='7'
+
+NAME=slurm-build
+DIST_DIR=./dist
+
+docker build --pull -t "$NAME" \
+             --file=./Dockerfile.ubuntu \
+             --build-arg SLURM_VERSION="$SLURM_VERSION" \
+             --build-arg UBUNTU_CODENAME="$UBUNTU_CODENAME" \
+            .
+docker ps -q -a -f "name=$NAME" | xargs docker rm -r
+docker create --name="$NAME" "$NAME"
+rm -rf "$DIST_DIR"
+docker cp "${NAME}:/dist" "$DIST_DIR"
+docker rm "$NAME"
